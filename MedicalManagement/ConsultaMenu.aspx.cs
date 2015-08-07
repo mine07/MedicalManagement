d7a21ff7957f7c951acf76bf906f8ee15dca5510
mine07 <cezar21@hotmail.es>
2015-08-07 18:13:34 -0500

    holu
    sadsa

--- MedicalManagement/ConsultaMenu.aspx.cs
+++ MedicalManagement/ConsultaMenu.aspx.cs
@@ -33,6 +33,8 @@ namespace MedicalManagement
                 GridViewFechaConsulta();
                 llenartxtantecedentesnotas();
                 GridViewProcedimiento();
+
+                llenarconsultas();
                                 
                 SqlConnection cnn;
                 cnn = new SqlConnection(conexion);
@@ -46,7 +48,6 @@ namespace MedicalManagement
                 if (Id_Consulta != 0)
                 {                    
                     LinkReceta.Visible = true;                     
-                    llenarconsultas();
                 }
                 else
                 {                    
