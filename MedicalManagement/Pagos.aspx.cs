beb8a2962d31d3c7c7dd618ed6be1da2361981f0
oscarcobb <osc100_000@hotmail.com>
2015-09-21 09:50:52 -0500

    Update 21/09/15
    By OscarCobb

--- MedicalManagement/Pagos.aspx.cs
+++ MedicalManagement/Pagos.aspx.cs
@@ -8,6 +8,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using MedicalManagement.Models;
 using MedicalManagement.Models.DTO;
+using System.Data;
 
 namespace MedicalManagement
 {
@@ -15,7 +16,12 @@ namespace MedicalManagement
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            loadData();
+            if (!IsPostBack) {
+                loadData();
+                AddDefaultFirstRecord();
+                AddDefaultFirstRecordHist();
+                
+            }
         }
 
         private void loadData()
@@ -27,9 +33,8 @@ namespace MedicalManagement
             Helpers h = new Helpers();
             var lFichas = h.GetAllParametized(queryFichas, oneFicha);
             foreach(var y in lFichas)
-            {
-                y._NombreCompleto = y.Nombre_FichaIdentificacion + " " + y.ApPaterno_FichaIdentificacion +
-                                     " " + y.ApMaterno_FichaIdentificacion;
+           {
+            y._NombreCompleto = y.Nombre_FichaIdentificacion + " " + y.ApPaterno_FichaIdentificacion + " " + y.ApMaterno_FichaIdentificacion;
             }
             var lConceptos = h.GetAllParametized(query, oneConcepto);
             ddlFichas.DataSource = lFichas;
@@ -37,6 +42,38 @@ namespace MedicalManagement
             ddlConceptos.DataSource = lConceptos;
             ddlConceptos.DataBind();
         }
+
+        private void loadHistPagos() {
+            DataSet ds = new DataSet();
+            try
+            {
+                var oneDiagnostico = new Tabla_Registro_PagosDTO();
+                string query = "Select * from Tabla_Registro_PagosB order by Id_Pagos";
+                Helpers h = new Helpers();
+                var lPagos = h.GetAllParametized(query, oneDiagnostico);
+                
+                if (lPagos.Count==0)
+                {
+
+                    ds.Tables[0].Rows.Add(ds.Tables[0].NewRow());
+                    gvHistPagos.DataSource = ds;
+                    gvHistPagos.DataBind();
+                    int columncount = gvHistPagos.Rows[0].Cells.Count;
+                    gvHistPagos.Rows[0].Cells.Clear();
+                    gvHistPagos.Rows[0].Cells.Add(new TableCell());
+                    gvHistPagos.Rows[0].Cells[0].ColumnSpan = columncount;
+                    gvHistPagos.Rows[0].Cells[0].Text = "SIN DATOS";
+                    gvHistPagos.Rows[0].Cells[0].Attributes.CssStyle["text-align"] = "Center";
+                    
+                }
+            }
+            catch (Exception ex) { 
+                
+                
+            }
+        }
+
+
 
         [WebMethod(EnableSession = true)]
         public static object Create(Tabla_Registro_PagosDTO record)
@@ -136,5 +173,128 @@ namespace MedicalManagement
             var lOpt = lFichas.Select(c => new { DisplayText = c._NombreCompleto, Value = c.Id_FichaIdentificacion }).OrderBy(s => s.DisplayText).ToList();
             return new { Result = "OK", Options = lOpt };
         }
+
+
+
+        protected void ddlConceptos_Selected(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM Tabla_Catalogo_ConceptoPago WHERE (Id_ConceptoPago=@Id_ConceptoPago)";
+            var oneConcepto = new Tabla_Catalogo_ConceptoPagoDTO();
+            Helpers h = new Helpers();
+            oneConcepto.Id_ConceptoPago = Convert.ToInt32(ddlConceptos.SelectedValue);
+            var lConceptos = h.GetAllParametized(query, oneConcepto);
+            foreach (var y in lConceptos)
+            {
+                txtCantidad.Text = "1";
+                txtPrecio.Text = y.PrecioUnitario.Trim();
+                txtDescuento.Text = "0.0";
+            }
+            popup.Show();
+        }
+
+        
+        protected void Add(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        protected void Edit(object sender, EventArgs e) {
+        }
+
+        protected void addCP(object sender, EventArgs e) {
+        }
+
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            
+                DataTable dtCurrentTable = (DataTable)ViewState["ProductsSold"];
+                DataRow drCurrentRow = null;
+                if (dtCurrentTable.Rows.Count > 0)
+                {
+                    for (int i = 1; i <= dtCurrentTable.Rows.Count; i++)
+                    {
+                        //Creating new row and assigning values
+                        drCurrentRow = dtCurrentTable.NewRow();
+                        drCurrentRow["Concepto"] = ddlConceptos.SelectedItem.Text.Trim();
+                        drCurrentRow["Cantidad"] = txtCantidad.Text;
+                        drCurrentRow["Total"] = txtPrecio.Text;
+                        drCurrentRow["Descuento"] = txtDescuento.Text;
+                                                
+                    }
+                    if (dtCurrentTable.Rows[0][0].ToString() == "")
+                    {
+                        dtCurrentTable.Rows[0].Delete();
+                        dtCurrentTable.AcceptChanges();
+                    }
+                    dtCurrentTable.Rows.Add(drCurrentRow);
+                    //Guardado de la gridviwe en ViewState 
+                    ViewState["ProductsSold"] = dtCurrentTable;
+                    gvConceptos.DataSource = dtCurrentTable;
+                    gvConceptos.DataBind();
+                }
+            }
+
+        private void AddDefaultFirstRecord()
+        {
+            //creating DataTable
+            DataTable dt = new DataTable();
+            DataRow dr;
+            dt.TableName = "ProductsSold";
+            //creating columns for DataTable
+            dt.Columns.Add(new DataColumn("Concepto", typeof(string)));
+            dt.Columns.Add(new DataColumn("Cantidad", typeof(string)));
+            dt.Columns.Add(new DataColumn("Total", typeof(string)));
+            dt.Columns.Add(new DataColumn("Descuento", typeof(string)));
+            dr = dt.NewRow();
+            dt.Rows.Add(dr);
+
+            ViewState["ProductsSold"] = dt;
+            gvConceptos.DataSource = dt;
+            gvConceptos.DataBind();
+        }
+
+        private void AddDefaultFirstRecordHist()
+        {
+            //creating DataTable
+            DataTable dt = new DataTable();
+            DataRow dr;
+            dt.TableName = "HISTORIAL";
+            //creating columns for DataTable
+            dt.Columns.Add(new DataColumn("Fecha", typeof(string)));
+            dt.Columns.Add(new DataColumn("Descripcion", typeof(string)));
+            dt.Columns.Add(new DataColumn("Origen", typeof(string)));
+            dt.Columns.Add(new DataColumn("Importe", typeof(string)));
+            dt.Columns.Add(new DataColumn("Pagado", typeof(string)));
+            dt.Columns.Add(new DataColumn("Debe", typeof(string)));
+            dt.Columns.Add(new DataColumn("FPP", typeof(string)));
+            dt.Columns.Add(new DataColumn("FP", typeof(string)));
+            dt.Columns.Add(new DataColumn("Factura", typeof(string)));
+            dr = dt.NewRow();
+            dt.Rows.Add(dr);
+
+            ViewState["HISTORIAL"] = dt;
+            gvHistPagos.DataSource = dt;
+            gvHistPagos.DataBind();
+        }
+
+        protected void CancelAdd(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnClean_Click(object sender, EventArgs e) { 
+            ViewState.Clear();
+            Session.RemoveAll();
+            Session.Clear();
+            Response.Redirect(Request.RawUrl);
+        }
+
+        protected void btnSavePagos_Click(object sender, EventArgs e) {
+
+        }
     }
-}
+
+
+    }
